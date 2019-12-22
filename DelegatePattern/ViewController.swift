65183ab9618f43fb8f962caeea27682d1aafdcfe
junhyuk <dejavukk1@gmail.com>
2019-12-22 23:36:30 +0900

    텍스트필드 UI 속성 구현.

--- DelegatePattern/ViewController.swift
+++ DelegatePattern/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var mainTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // 안내 메시지
+        self.mainTextField.placeholder = "값을 입력."
+        // 키보드 타입 영문자 패드
+        self.mainTextField.keyboardType = UIKeyboardType.alphabet
+        // 키보드 스타일 어둡게
+        self.mainTextField.keyboardAppearance = UIKeyboardAppearance.dark
+        // 리턴키 타입은 "Join"
+        self.mainTextField.returnKeyType = UIReturnKeyType.join
+        // 리턴키 자동 활성화 "On"
+        self.mainTextField.enablesReturnKeyAutomatically = true
+        
+        // MARK: - 스타일 설정
+        
+        // 테두리 스타일 - 직선
+        self.mainTextField.borderStyle = UITextField.BorderStyle.line
+        // 배경 색상
+        self.mainTextField.backgroundColor = UIColor(white: 0.87, alpha: 1.0)
+        // 수직 방향으로 텍스트가 가운데 정렬
+        self.mainTextField.contentVerticalAlignment = .center
+        // 수평 방향으로 텍스트가 가운데 정렬되도록
+        self.mainTextField.contentHorizontalAlignment = .center
+        // 테두리 색상을 회색으로
+        self.mainTextField.layer.borderColor = UIColor.darkGray.cgColor
+        // 테두리 두께 설정
+        self.mainTextField.layer.borderWidth = 2.0
+        
     }
 
 
